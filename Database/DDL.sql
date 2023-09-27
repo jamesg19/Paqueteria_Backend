58e156878a60051070dbc0594fc218cf6e1ad19f
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2023-09-26 22:14:42 -0600

    Data

---
+++ Database/DDL.sql
@@ -0,0 +1,149 @@
+USE `paqueteria` ;
+
+
+-- Agregar Departamentos de Guatemala
+INSERT INTO departamentos (id, nombre)
+VALUES
+    (1, 'Guatemala'),
+    (2, 'Baja Verapaz'),
+    (3, 'Alta Verapaz'),
+    (4, 'El Progreso'),
+    (5, 'Izabal'),
+    (6, 'Zacapa'),
+    (7, 'Chiquimula'),
+    (8, 'Santa Rosa'),
+    (9, 'Jalapa'),
+    (10, 'Jutiapa'),
+    (11, 'Sacatepequez'),
+    (12, 'Chimaltenango'),
+    (13, 'Escuintla'),
+    (14, 'Suchitepequez'),
+    (15, 'Retalhuleu'),
+    (16, 'San Marcos'),
+    (17, 'Huehuetenango'),
+    (18, 'Quetzaltenango'),
+    (19, 'Totonicapan'),
+    (20, 'Quiche'),
+    (21, 'Solola'),
+    (22, 'Suchitepequez'),
+    (23, 'Peten');
+
+
+-- Agregar Sucursales en Quetzaltenango
+INSERT INTO sucursales (id, nombre, direccion, departamento_id)
+VALUES
+    (1, 'Sucursal Xela', 'Dirección Xela 1', 18),
+    (2, 'Sucursal Guatemala', 'Dirección Guatemala 1', 1),
+    (3, 'Sucursal Peten', 'Dirección Peten 1', 23),
+    (4, 'Sucursal Escuintla', 'Dirección Escuintla 1', 13);
+
+
+-- Agregar Roles
+INSERT INTO roles (id, nombre_rol)
+VALUES
+    (1, 'Secretaria/o'),
+    (2, 'Bodega'),
+    (3, 'Conserje'),
+    (4, 'Operaciones de Logística'),
+    (5, 'Guardia'),
+    (6, 'Piloto'),
+    (7, 'Auxiliar de Piloto'),
+    (8, 'Gerente');
+
+-- Agregar Personal en Sucursal Quetzaltenango (ID: 1)
+INSERT INTO personal (id, nombre, apellido, sucursal_id, rol_id)
+VALUES
+    (1, 'Juan', 'Pérez', 1, 1), -- Secretaria/o
+    (2, 'María', 'González', 1, 2), -- Bodega
+    (3, 'Carlos', 'Rodríguez', 1, 3), -- Conserje
+    (4, 'Luisa', 'Sánchez', 1, 4), -- Operaciones de Logística
+    (5, 'Pedro', 'Martínez', 1, 5), -- Guardia
+    (6, 'Ana', 'López', 1, 6), -- Piloto
+    (7, 'Roberto', 'Díaz', 1, 7), -- Auxiliar de Piloto
+    (8, 'James', 'Gramajo', 1, 8); -- Gerente
+
+-- Agregar Personal en Sucursal Guatemala (ID: 2)
+INSERT INTO personal (id, nombre, apellido, sucursal_id, rol_id)
+VALUES
+    (9, 'Sofía', 'Hernández', 2, 1), -- Secretaria/o
+    (10, 'Esteban', 'Ramírez', 2, 2), -- Bodega
+    (11, 'Laura', 'Gómez', 2, 3), -- Conserje
+    (12, 'Andrés', 'Paz', 2, 4), -- Operaciones de Logística
+    (13, 'Raúl', 'Jiménez', 2, 5), -- Guardia
+    (14, 'Isabel', 'Torres', 2, 6), -- Piloto
+    (15, 'Antonio', 'Vargas', 2, 7), -- Auxiliar de Piloto
+    (16, 'William', 'Umaña', 2, 8); -- Gerente
+
+-- Agregar Personal en Sucursal Peten (ID: 3)
+INSERT INTO personal (id, nombre, apellido, sucursal_id, rol_id)
+VALUES
+    (17, 'Laura', 'Pérez', 3, 1), -- Secretaria/o
+    (18, 'Carmen', 'González', 3, 2), -- Bodega
+    (19, 'Angela', 'Rodríguez', 3, 3), -- Conserje
+    (20, 'Isabel', 'Sánchez', 3, 4), -- Operaciones de Logística
+    (21, 'Luis', 'Martínez', 3, 5), -- Guardia
+    (22, 'Alberto', 'López', 3, 6), -- Piloto
+    (23, 'Cesar', 'Díaz', 3, 7), -- Auxiliar de Piloto
+    (24, 'David', 'Martinez', 3, 8); -- Gerente
+
+-- Agregar Personal en Sucursal Escuintlad (ID: 4)
+INSERT INTO personal (id, nombre, apellido, sucursal_id, rol_id)
+VALUES
+    (25, 'Enma', 'Hernández', 4, 1), -- Secretaria/o
+    (26, 'Esteban', 'Ramírez', 4, 2), -- Bodega
+    (27, 'Sandra', 'Gómez', 4, 3), -- Conserje
+    (28, 'Andrea', 'Paz', 4, 4), -- Operaciones de Logística
+    (29, 'Sergio', 'Jiménez', 4, 5), -- Guardia
+    (30, 'Leonel', 'Torres', 4, 6), -- Piloto
+    (31, 'Anders', 'Vargas', 4, 7), -- Auxiliar de Piloto
+    (32, 'Ronaldo', 'Méndez', 4, 8); -- Gerente
+
+
+-- Agregar Tarifas por Envío
+INSERT INTO tarifa_envio (id, cantidad_envios, tarifa)
+VALUES
+    (1, 1, 10.00),
+    (2, 5, 8.00),
+    (3, 10, 7.00),
+    (4, 20, 6.00),
+    (5, 50, 5.00),
+    (6, 100, 4.00);
+
+
+
+-- Envios de Xela
+INSERT INTO envios (fecha_envio, sucurlar_origen_id, sucurlar_destino_id, cantidad_paquetes, tarifa_id, fecha_entrega)
+VALUES
+    ('2023-09-26', 1, 2, 3, 1, '2023-09-30'), --
+    ('2023-09-27', 1, 2, 5, 1, '2023-10-01'), --
+    ('2023-09-28', 1, 3, 8, 2, '2023-10-02'),
+    ('2023-09-29', 1, 4, 3, 1, '2023-10-03'),
+    ('2023-09-30', 1, 3, 3, 1, '2023-10-04');
+
+-- Envios de Guate
+INSERT INTO envios (fecha_envio, sucurlar_origen_id, sucurlar_destino_id, cantidad_paquetes, tarifa_id, fecha_entrega)
+VALUES
+    ('2023-09-26', 2, 1, 5, 1, '2023-09-30'),
+    ('2023-09-27', 2, 1, 8, 2, '2023-10-01'),
+    ('2023-09-28', 2, 3, 3, 1, '2023-10-02'),
+    ('2023-09-29', 2, 4, 4, 1, '2023-10-03'),
+    ('2023-09-30', 2, 4, 5, 1, '2023-10-04');
+
+
+-- Envios de Peten
+INSERT INTO envios (fecha_envio, sucurlar_origen_id, sucurlar_destino_id, cantidad_paquetes, tarifa_id, fecha_entrega)
+VALUES
+    ('2023-09-26', 3, 1, 1, 1, '2023-09-30'), --
+    ('2023-09-27', 3, 1, 1, 1, '2023-10-01'), --
+    ('2023-09-28', 3, 4, 2, 1, '2023-10-02'),
+    ('2023-09-29', 3, 4, 1, 1, '2023-10-03'),
+    ('2023-09-30', 3, 2, 5, 1, '2023-10-04');
+
+-- Envios de Escuintla
+INSERT INTO envios (fecha_envio, sucurlar_origen_id, sucurlar_destino_id, cantidad_paquetes, tarifa_id, fecha_entrega)
+VALUES
+    ('2023-09-26', 4, 1, 2, 1, '2023-09-30'), --
+    ('2023-09-27', 4, 2, 2, 1, '2023-10-01'), --
+    ('2023-09-28', 4, 2, 4, 1, '2023-10-02'),
+    ('2023-09-29', 4, 3, 5, 1, '2023-10-03'),
+    ('2023-09-30', 4, 3, 1, 1, '2023-10-04');
--- Database/DML.sql
+++ Database/DML.sql
@@ -3,56 +3,97 @@ DROP SCHEMA IF EXISTS `paqueteria`;
 CREATE SCHEMA `paqueteria`;
 USE `paqueteria` ;
 
+-- Tabla de departamentos
+CREATE TABLE departamentos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
+
 -- Tabla de Sucursales
-CREATE TABLE Sucursales (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(255),
-    direccion VARCHAR(255),
-    departamento VARCHAR(50)
-);
+CREATE TABLE sucursales (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    departamento_id INT NOT NULL,
+    FOREIGN KEY (departamento_id) REFERENCES departamentos(id)
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
+
+
+
 
 -- Tabla de Roles
-CREATE TABLE Roles (
-    id INT PRIMARY KEY,
-    nombre_rol VARCHAR(50)
-);
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_rol VARCHAR(50) NOT NULL
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
 
 -- Tabla de Personal de Sucursales
 CREATE TABLE personal (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(50),
-    apellido VARCHAR(50),
-    sucursal_id INT,
-    rol_id INT,
-    FOREIGN KEY (sucursal_id) REFERENCES Sucursales(id),
-    FOREIGN KEY (rol_id) REFERENCES Roles(id)
-);
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    sucursal_id INT NOT NULL,
+    rol_id INT NOT NULL,
+
+    FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),
+    FOREIGN KEY (rol_id) REFERENCES roles(id)
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
 
 -- Tabla de Tarifas por Envío
 CREATE TABLE tarifa_envio (
-    id INT PRIMARY KEY,
-    cantidad_envios INT,
-    tarifa DECIMAL(10, 2)
-);
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cantidad_envios INT NOT NULL,
+    tarifa DECIMAL(10, 2) NOT NULL
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
 
 -- Tabla de Envíos
 CREATE TABLE envios (
-    envio_id INT PRIMARY KEY,
-    fecha_envio DATE,
-    sucurlar_origen_id INT,
-    sucurlar_destino_id INT,
-    cantidad_paquetes INT,
-    tarifa_id INT,
-    FOREIGN KEY (sucurlar_origen_id) REFERENCES Sucursales(id),
-    FOREIGN KEY (sucurlar_destino_id) REFERENCES Sucursales(id),
+    envio_id INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_envio DATE NOT NULL,
+    sucurlar_origen_id INT NOT NULL,
+    sucurlar_destino_id INT NOT NULL,
+    cantidad_paquetes INT NOT NULL,
+    tarifa_id INT NOT NULL,
+    fecha_entrega DATE NOT NULL,
+    FOREIGN KEY (sucurlar_origen_id) REFERENCES sucursales(id),
+    FOREIGN KEY (sucurlar_destino_id) REFERENCES sucursales(id),
     FOREIGN KEY (tarifa_id) REFERENCES tarifa_envio(id)
-);
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
 
 -- Tabla de Vehículos
 CREATE TABLE vehiculos (
-    vehiculo_id INT PRIMARY KEY,
-    tipo VARCHAR(100),
-    tonelaje DECIMAL(5, 2),
-    sucursal INT, -- A la sucursal que pertenece el vehiculo
-    FOREIGN KEY (sucursal) REFERENCES Sucursales(id)
-);
+    vehiculo_id INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(100) NOT NULL,
+    tonelaje DECIMAL(5, 2) NOT NULL,
+    sucursal INT NOT NULL, -- A la sucursal que pertenece el vehiculo
+    FOREIGN KEY (sucursal) REFERENCES sucursales(id)
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
+
+-- Tabla de Seguimiento de la ruta del envío
+CREATE TABLE seguimiento_envios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    envio_id INT NOT NULL,
+    sucursal_id INT,
+    vehiculo_id INT,
+    descripcion VARCHAR(255),
+
+    FOREIGN KEY (envio_id) REFERENCES envios(envio_id),
+    FOREIGN KEY (sucursal_id) REFERENCES sucursales(id),
+    FOREIGN KEY (vehiculo_id) REFERENCES vehiculos(vehiculo_id)
+) ENGINE=InnoDB AUTO_INCREMENT = 1;
+
+
+
+
+-- Tabla de Distancias entre Tiendas
+CREATE TABLE distancia_tiendas (
+    sucursal_origen INT NOT NULL,
+    sucursal_destino INT NOT NULL,
+    distancia DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (sucursal_origen,sucursal_destino),
+    FOREIGN KEY (sucursal_origen) REFERENCES sucursales(id),
+    FOREIGN KEY (sucursal_destino) REFERENCES sucursales(id)
+)ENGINE=InnoDB AUTO_INCREMENT = 1;
+
+
+
